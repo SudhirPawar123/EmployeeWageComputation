2a90f58e6e467ec54e0e93d5b0b5ae1ad93714d5
SudhirPawar123 <sudhipawar329@gmail.com>
2024-09-18 12:41:44 +0530

    calculateEmployeeWageBasedOnCondition feature done

---
+++ EmployeeWageCalculationBasedOnCondition.java
@@ -0,0 +1,5 @@
+public class EmployeeWageCalculationBasedOnCondition {
+    public static void main(String[] args) {
+        System.out.println("Here we are calculating wage based on some condition");
+    }
+}
