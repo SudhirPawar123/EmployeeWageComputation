04d0b53f701dbaaefba5e313742af9772676ce0e
SudhirPawar123 <sudhipawar329@gmail.com>
2024-09-18 12:23:44 +0530

    addPartTimeEmployeeWageCalculation feature in this branch

---
+++ EmployeePartTimeWageCalculation.java
@@ -0,0 +1,5 @@
+public class EmployeePartTimeWageCalculation {
+    public static void main(String[] args) {
+        System.out.println("Here we are calculating a employee part time job wage");
+    }
+}
