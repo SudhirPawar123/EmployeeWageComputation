6ede0d0791f3e17fabee755d49211ae41b9fabb9
SudhirPawar123 <sudhipawar329@gmail.com>
2024-09-18 12:48:03 +0530

    we done with final feature i.e employee wage calculation

---
+++ EmployeeWageCalculationFinalFeature.java
@@ -0,0 +1,5 @@
+public class EmployeeWageCalculationFinalFeature {
+    public static void main(String[] args) {
+        System.out.println("we done with final employee wage calculation part");
+    }
+}
